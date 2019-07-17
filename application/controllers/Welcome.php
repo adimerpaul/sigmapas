0edcc83c6b1069094389bfa0caf35317df8521fb
adimerpaul <adimer101@email.com>
2019-07-17 14:49:42 -0400

    mofificado

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -7,15 +7,18 @@ class Welcome extends CI_Controller {
         if ( isset($_POST['lat']) && isset($_POST['long'])){
             $lat= $_POST['lat'];
             $long= $_POST['long'];
+            $bufer="si";
             $zum=19;
         }else{
             $lat= -17.9647;
             $long= -67.106;
             $zum=14;
+            $bufer="no";
         }
         $data['lat']=$lat;
         $data['long']=$long;
         $data['zum']=$zum;
+        $data['bufer']=$bufer;
         $this->load->view('templates/header2');
         $this->load->view('main',$data);
         $this->load->view('templates/footer');
--- application/views/main.php
+++ application/views/main.php
@@ -1,12 +1,95 @@
+<?php if ($bufer=="si"):?>
+<style>
+    #floating-panel {
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+        text-align: center;
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 10px;
+    }
+</style>
+<div id="floating-panel">
+    <input onclick="aumentar();" type=button value="+">
+    <input onclick="disminuir();" type=button value="-">
+</div>
+<?php endif;?>
+
 <div style="width: 100%;height: 500px" id="map"></div>
 <script>
-      var map;
+    function aumentar() {
+        cityCircle.setMap(null);
+        ra=ra+5;
+        cityCircle= new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: {lat: <?=$lat?>, lng: <?=$long?>},
+            radius:  ra
+        });
+    }
+    function disminuir() {
+        cityCircle.setMap(null);
+        ra=ra-5;
+        cityCircle= new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: {lat: <?=$lat?>, lng: <?=$long?>},
+            radius:  ra
+        });
+    }
+      var map, infoWindow, cityCircle,ra=80;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: <?=$lat?>, lng: <?=$long?>},
           zoom: <?=$zum?>,
           disableDoubleClickZoom: true,
         });
+          infoWindow = new google.maps.InfoWindow;
+
+          // Try HTML5 geolocation.
+          if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                  var pos = {
+                      lat: position.coords.latitude,
+                      lng: position.coords.longitude
+                  };
+                  infoWindow.setPosition(pos);
+                  infoWindow.setContent('Ubicación encontrada.');
+                  infoWindow.open(map);
+                  map.setCenter(pos);
+              }, function() {
+                  handleLocationError(true, infoWindow, map.getCenter());
+              });
+          } else {
+              // Browser doesn't support Geolocation
+              handleLocationError(false, infoWindow, map.getCenter());
+          }
+
+          <?php
+          if ($bufer=="si"){
+              echo " cityCircle= new google.maps.Circle({
+                        strokeColor: '#FF0000',
+                        strokeOpacity: 0.8,
+                        strokeWeight: 2,
+                        fillColor: '#FF0000',
+                        fillOpacity: 0.35,
+                        map: map,
+                        center: {lat: $lat, lng: $long},
+                        radius:  ra
+                      });";
+          }
+          ?>
+
           <?php
           $query=$this->db->query("SELECT * FROM COLEGIO WHERE estado='ACTIVO'");
           foreach($query->result() as $row){
@@ -14,7 +97,9 @@
                     position: {lat: $row->lat, lng: $row->long},
                     map: map,
                     title: '$row->nombre'
-                });";
+                });
+                attachSecretMessage(marker, '$row->nombre');
+                ";
           }
           ?>
 
@@ -60,7 +145,22 @@
 
 
       }
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+          infoWindow.setPosition(pos);
+          infoWindow.setContent(browserHasGeolocation ?
+              'Error: El servicio de geolocalizacion no esta diponible.' :
+              'Error: Your browser doesn\'t support geolocation.');
+          infoWindow.open(map);
+      }
+      function attachSecretMessage(marker, secretMessage) {
+          var infowindow = new google.maps.InfoWindow({
+              content: secretMessage
+          });
 
+          marker.addListener('click', function() {
+              infowindow.open(marker.get('map'), marker);
+          });
+      }
     </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAgjOr5cGSqjKTsh5lQO5yqboLz9YYbbIE&callback=initMap"
-    async defer></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAFPv-DAgFBveRbqjSlqufPSLB1E3avBMI&callback=initMap"
+        async defer></script>
